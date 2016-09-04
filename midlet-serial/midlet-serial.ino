6f814e4f8b206b34a7760c64536c3d363ba79b8c
Hansel Ramos <hansel.ramos@yuxipacific.com>
2016-09-03 20:40:20 -0500

    Added flags

--- midlet-serial/midlet-serial.ino
+++ midlet-serial/midlet-serial.ino
@@ -1,5 +1,35 @@
+int incomingByte = 0;   // for incoming serial data
+
 void setup() {
+  pinMode(7,OUTPUT);
+  digitalWrite(7, LOW);
+  pinMode(8,OUTPUT);
+  digitalWrite(8, LOW);
+  //pinMode(13, OUTPUT);
+  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() {
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+
+    if(incomingByte==65){//A                 
+      digitalWrite(8, HIGH);
+    }else if(incomingByte==97){//a
+      digitalWrite(8, LOW);
+    }else if(incomingByte==66){//B
+      digitalWrite(7, HIGH);
+    }else if(incomingByte==98){//b
+      digitalWrite(7, LOW);
+    }
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+
+    }
+      
 }
